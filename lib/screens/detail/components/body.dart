9b8397d2cb0888903f53e59798392acbba50d78b
daiphuc <phamdaiphuc001@gmail.com>
2022-02-16 00:27:33 +0700

    add detaild update

--- lib/screens/detail/components/body.dart
+++ lib/screens/detail/components/body.dart
@@ -1,4 +1,5 @@
 import 'package:app_new/constants.dart';
+import 'package:app_new/screens/detail/components/product_title_with_image.dart';
 import 'package:flutter/material.dart';
 import 'package:app_new/models/Product.dart';
 
@@ -11,35 +12,98 @@ class Body extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
-          
           Container(
-            height: size.height, 
+            height: size.height,
             child: Stack(
-              children:[
+              children: [
                 Container(
-                  margin: EdgeInsets.only(top: 120.0),
-                  height:500,
+                  margin: EdgeInsets.only(top: size.height * 0.4),
+                  padding: EdgeInsets.only(
+                      top: size.height * 0.12,
+                      left: kDefaultPaddin,
+                      right: kDefaultPaddin),
+                  height: 500,
                   decoration: BoxDecoration(
-                    color: Colors.white,
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(24),
+                          topRight: Radius.circular(24)
+                          )
+                          ),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text('Color'),
+
+                                Row(
+                                  children: <Widget>[
+                                    colorDot(color: Color(0xFF356C95)),
+                                    colorDot(color: Color(0xFF356C95)),
+                                    colorDot(color: Color(0xFF356C95))
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: RichText(
+                                text: TextSpan(
+                                    style: TextStyle(color: kTextColor),
+                                    children: [
+                                  TextSpan(
+                                    text: 'Size\n',
+                                  ),
+                                  TextSpan(text: "${product.size} cm",style: Theme.of(context).textTheme.headline5?.copyWith(fontWeight:FontWeight.bold))
+                                ])),
+                          )
+                        ],
+                      ),
+                      Text(product.description)
+                    ],
                   ),
                 ),
-                    Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('xin chao\n',style: TextStyle(color: Colors.white),
-                      ),
-                      Text(product.title,style: TextStyle(color: Colors.white),
-                      ),
-        
-                       ],
-            ),
-          ),
+                Producttitleimage(product: product),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class colorDot extends StatelessWidget {
+  final color;
+  final isSelected;
+  const colorDot({
+    Key? key,
+    required this.color,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: kDefaultPaddin / 4,
+        right: kDefaultPaddin / 2,
+      ),
+      padding: EdgeInsets.all(2.5),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: color,
+        ),
+      ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(color: color, shape: BoxShape.circle),
       ),
     );
   }

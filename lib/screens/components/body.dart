5322340a6f7a278cb1c1fe01fcfa8cac01fb76ef
Phạm đại Phúc <phamdaiphuc@Phams-MacBook-Pro.local>
2022-02-21 16:47:31 +0700

    class itemcard

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,12 +1,11 @@
-
 import 'package:flutter/material.dart';
 import 'package:app_new/constants.dart';
 import 'package:app_new/models/Product.dart';
 import 'package:app_new/screens/detail/detais_screen.dart';
 import 'itemcard.dart';
 import 'categories.dart';
+
 class Body extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
@@ -21,32 +20,41 @@ class Body extends StatelessWidget {
         ),
       ),
       Categories(),
-      Expanded(
-        
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-          child: GridView.builder(
-              itemCount: products.length,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: kDefaultPaddin,
-                crossAxisSpacing: kDefaultPaddin,
-                childAspectRatio: 0.75,
-              ),
-               itemBuilder: (context, index) => ItemCard(
-                      product: products[index],
-                      press: () => Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DetailsScreen(
-                              product: products[index]  , 
-                            ),
-                          )),
-                    )),
-                              ),
-        ),
-      
+      itemcard(),
     ]);
+  }
+}
+
+class itemcard extends StatelessWidget {
+  const itemcard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+        child: GridView.builder(
+            itemCount: products.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              mainAxisSpacing: kDefaultPaddin,
+              crossAxisSpacing: kDefaultPaddin,
+              childAspectRatio: 0.75,
+            ),
+            itemBuilder: (context, index) => ItemCard(
+                  product: products[index],
+                  press: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailsScreen(
+                          product: products[index],
+                        ),
+                      )),
+                )),
+      ),
+    );
   }
 }
 
